f2d53017e6d1e7f28cdf07b78c6dc851911ccf66
SurajdeoMehta <113961559+SurajdeoMehta@users.noreply.github.com>
2022-09-25 18:12:10 +0530

    Create max of four.c

---
+++ Assignment 2/max of four.c
@@ -0,0 +1,28 @@
+https://www.hackerrank.com/challenges/functions-in-c/problem?isFullScreen=true
+
+
+#include <stdio.h>
+int max(int a,int b,int c,int d)
+{
+    int greatest;
+    if(a>b && a>d && a>c)
+    greatest=a;
+    else if (b>a && b>c && b>d) 
+    greatest=b;
+    else if (c>a && c>b && c>d)
+    greatest=c;
+    else 
+greatest=d;
+    return greatest;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int greatest = max(a, b, c, d);
+    printf("%d", greatest);
+    
+    return 0;
+}
+
