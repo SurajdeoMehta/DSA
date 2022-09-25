6aefbe9a8a79a29f6433d23b66c3fd4c5e2df1f3
SurajdeoMehta <113961559+SurajdeoMehta@users.noreply.github.com>
2022-09-25 18:00:39 +0530

    Create characters and strings.c

---
+++ Assignment 2/characters and strings.c
@@ -0,0 +1,20 @@
+https://www.hackerrank.com/challenges/playing-with-characters/problem?isFullScreen=true
+
+
+--#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char ch;
+char s[20],sen[100];
+//string s,sen;
+scanf("%c",&ch);
+scanf("%s",s);
+scanf("\n");
+gets(sen);
+printf("%c \n",ch);
+printf("%s \n",s);
+puts(sen);
