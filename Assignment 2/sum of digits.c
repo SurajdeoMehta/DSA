4350fe3e50076a4e076aee37196a8ce912268cfc
SurajdeoMehta <113961559+SurajdeoMehta@users.noreply.github.com>
2022-09-25 18:13:06 +0530

    Create sum of digits.c

---
+++ Assignment 2/sum of digits.c
@@ -0,0 +1,25 @@
+https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem?isFullScreen=true
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+ {
+int n;
+scanf("%d",&n);
+int a[n] ,sum=0;
+for(int i=0;i<n;i++)
+scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+    {
+ sum=sum+a[i];
+    }
+    printf("%d",sum);
+ return 0;
+ }
+
+
+
